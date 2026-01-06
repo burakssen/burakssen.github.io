4fa33eee4576bd1e82015f6a7018083ae783549a
Burak Şen <buraksen7@hotmail.com>
2026-01-06 15:11:16 +0100

    Update

--- src/data/tech-stack.ts
+++ src/data/tech-stack.ts
@@ -1,25 +1,26 @@
 import {
   SiReact, SiTypescript, SiCplusplus, SiC, SiRaylib, SiZig, SiTailwindcss,
-  SiJavascript, SiVite, SiShadcnui, SiCmake, SiSwift, SiPython, SiPostgresql
+  SiJavascript, SiVite, SiShadcnui, SiCmake, SiSwift, SiPython, SiPostgresql,
+  SiDjango, SiHtmx,
 } from "@icons-pack/react-simple-icons";
 
 export const techStack = {
   professional: [
+    { icon: SiPython, name: "Python" },
+    { icon: SiDjango, name: "Django" },
+    { icon: SiHtmx, name: "HTMX" },
+    { icon: SiTailwindcss, name: "Tailwind" },
+    { icon: SiPostgresql, name: "PostgreSQL" },
     { icon: SiReact, name: "React" },
     { icon: SiTypescript, name: "TypeScript" },
     { icon: SiJavascript, name: "JavaScript" },
-    { icon: SiVite, name: "Vite" },
-    { icon: SiShadcnui, name: "Shadcn" },
-    { icon: SiTailwindcss, name: "Tailwind" },
-    { icon: SiPython, name: "Python" },
-    { icon: SiPostgresql, name: "PostgreSQL" }
   ],
   hobby: [
+    { icon: SiZig, name: "Zig" },
     { icon: SiCplusplus, name: "C++" },
     { icon: SiC, name: "C" },
     { icon: SiRaylib, name: "Raylib" },
     { icon: SiCmake, name: "CMake" },
     { icon: SiSwift, name: "Swift" },
-    { icon: SiZig, name: "Zig" }
   ]
 };
