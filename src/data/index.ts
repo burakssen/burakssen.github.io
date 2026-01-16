ea44154c31ca667efb34dc0083a2d3865a606186
Burak Şen <burak.sen@tum.de>
2026-01-16 14:02:27 +0100

    Update website

--- src/data/index.ts
+++
@@ -1,6 +0,0 @@
-import { Item } from "../components/project-card";
-import { recentWorks } from "../recent-works";
-import { recentProfessionalExperiences } from "../professional-experiences";
-
-export type { Item as ProjectItem };
-export { recentWorks, recentProfessionalExperiences };
